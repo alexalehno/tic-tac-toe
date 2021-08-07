3a9bbd0d646ccfd96d9b52a55475410b513af907
alexalehno <alekswakeup@gmail.com>
2021-08-07 16:39:02 +0300

    solve tic-tac-toe

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,68 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.winner = null;
+    this.step = true;
+    this.noTurns = false;
 
+    this.field = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+  }
+
+  getCurrentPlayerSymbol() {
+    return this.step ? "x" : "o";
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    const winComb = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    if (this.field[rowIndex][columnIndex] === null) {
+      this.field[rowIndex][columnIndex] = this.step ? "x" : "o";
+      this.step = !this.step;
     }
 
-    getCurrentPlayerSymbol() {
+    const field = this.field.flat();
 
+    for (let i = 0; i < winComb.length; i++) {
+      const [a, b, c] = winComb[i];
+      if (field[a] && field[a] === field[b] && field[a] === field[c]) {
+        this.winner = field[a];
+      }
     }
 
-    nextTurn(rowIndex, columnIndex) {
+    this.noTurns = !this.field.some((el) => el.includes(null));
+  }
 
-    }
+  isFinished() {
+    return this.winner || this.noTurns ? true : false;
+  }
 
-    isFinished() {
+  getWinner() {
+    return this.winner;
+  }
 
-    }
+  noMoreTurns() {
+    return this.noTurns;
+  }
 
-    getWinner() {
+  isDraw() {
+    return !this.winner && this.noTurns ? true : false;
+  }
 
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.field[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
